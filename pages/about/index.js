5130ce9bafed0fd6f0eb9c35bff31c5292fe3c3a
Daimeng Chen <daimengchen@gmail.com>
2022-02-18 20:53:53 -0500

    fix more styling

--- pages/about/index.js
+++ pages/about/index.js
@@ -26,7 +26,9 @@ export default function About() {
       <ContentCard id="contact" className="pd-2">
         <div className="lanes">
           <div className="lane">
-            <Phone style={{ width: '2rem', height: '2rem', backgroundColor: "lightpink" }} className="circled" />
+            <div className="circled" style={{ width: '2rem', height: '2rem', backgroundColor: "lightpink" }}>
+              <Phone />
+            </div>
             <div style={{ fontWeight: 'bold' }}>
               電話
             </div>
@@ -38,7 +40,9 @@ export default function About() {
             </div>
           </div>
           <div className="lane">
-            <Facebook style={{ width: '2rem', height: '2rem', backgroundColor: "steelblue" }} className="circled" />
+            <div className="circled" style={{ width: '2rem', height: '2rem', backgroundColor: "steelblue" }}>
+              <Facebook />
+            </div>
             <div style={{ fontWeight: 'bold' }}>
               Facebook
             </div>
@@ -47,7 +51,9 @@ export default function About() {
             </a>
           </div>
           <div className="lane">
-            <Mail style={{ width: '2rem', height: '2rem', backgroundColor: "skyblue" }} className="circled" />
+            <div className="circled" style={{ width: '2rem', height: '2rem', backgroundColor: "skyblue" }}>
+              <Mail />
+            </div>
             <div style={{ fontWeight: 'bold' }}>
               電郵
             </div>
